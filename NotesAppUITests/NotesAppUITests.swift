449364827e29e1cd3baf2638d85215d004eb12a6
lizterangelo <lmabrau@up.edu.ph>
2025-05-12 18:30:14 +0800

    Revert "test: Add UI test for adding new note"
    This reverts commit 3a0785c4f4875303c8447d0cb73bfcc2ce04f938.

--- NotesAppUITests/NotesAppUITests.swift
+++ NotesAppUITests/NotesAppUITests.swift
@@ -31,35 +31,4 @@ final class NotesAppUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    @MainActor
-    func testAddNewNote() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        // Tap the add button
-        let addButton = app.buttons["addButton"]
-        XCTAssertTrue(addButton.exists, "Add button should exist")
-        addButton.tap()
-
-        // Enter title
-        let titleTextField = app.textFields.element(boundBy: 0) // Assuming the first text field is the title
-        XCTAssertTrue(titleTextField.exists, "Title text field should exist")
-        titleTextField.tap()
-        titleTextField.typeText("Thesis test by lizter")
-
-        // Enter content
-        let contentTextView = app.textViews["noteContentTextView"]
-        XCTAssertTrue(contentTextView.exists, "Content text view should exist")
-        contentTextView.tap()
-        contentTextView.typeText("this is a test")
-
-        // Tap the done button
-        let doneButton = app.navigationBars.buttons["Done"]
-        XCTAssertTrue(doneButton.exists, "Done button should exist")
-        doneButton.tap()
-
-        // Verify the new note appears in the list
-        let newNoteCell = app.tables.cells.staticTexts["Thesis test by lizter"]
-        XCTAssertTrue(newNoteCell.waitForExistence(timeout: 5), "New note with title 'Thesis test by lizter' should appear in the list")
-    }
 }
